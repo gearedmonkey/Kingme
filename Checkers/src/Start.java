d83c5d785e4beab9f2998388fa98d847ca9bff80
bridgecode <xmyn4ever1@gmail.com>
2014-06-16 12:30:30 -0500

    vjk

--- Checkers/src/Start.java
+++ Checkers/src/Start.java
@@ -5,7 +5,7 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("talk dirty to me");
-		System.out.println("i dont understand")
+		System.out.println("i dont understand");
 	}
 
 }
