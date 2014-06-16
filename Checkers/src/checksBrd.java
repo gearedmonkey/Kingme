a0fe982c60b0190d40d212c737e6e7e7d4832827
bridgecode <xmyn4ever1@gmail.com>
2014-06-16 18:21:08 -0500

    f

--- Checkers/src/checksBrd.java
+++ Checkers/src/checksBrd.java
@@ -1,8 +1,15 @@
 package src;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 public class checksBrd {
 
@@ -11,13 +18,12 @@ public class checksBrd {
 	JLayeredPane duck;
 
 	public checksBrd() {
-		outline.setVisible(true);
 		JPanel Board = new JPanel();
 		duck = new JLayeredPane();
 		duck.add(Board, JLayeredPane.DEFAULT_LAYER);
 		outline = new JFrame("Game");
 		outline.setSize(500, 500);
-		outline.setLayout(new GridLayout(8, 8));
+		outline.setLayout(new GridLayout(8, 9));
 
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
@@ -26,11 +32,14 @@ public class checksBrd {
 				if ((i + j) % 2 == 0) {
 					spaces[i][j].setBackground(Color.black);
 				} else {
-					spaces[i][j].setBackground(Color.white);
+					spaces[i][j].setBackground(Color.red);
 				}
 				outline.add(spaces[i][j]);
 			}
 		}
+
+		
+		outline.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		outline.setVisible(true);
 
 	}
