7722fb49073ce273e9bd11dbf46079c85f497685
bridgecode <xmyn4ever1@gmail.com>
2014-06-16 13:01:28 -0500

    i made food

--- Checkers/src/Start.java
+++ Checkers/src/Start.java
@@ -5,8 +5,11 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("talk dirty to me");
-		System.out.println("i dont understand");
 		System.out.println("TEST");
+		
+		int toast = 90;
+		double bacon = 7.89;
+		String breakfast;
 	}
 
 }
